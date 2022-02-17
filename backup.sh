a704225be76b305ba309f40135124e5cd9026254
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2022-02-17 04:57:02 +0100

    Modifications for backing up disk C on the Windows desktop PC

--- backup.sh
+++ backup.sh
@@ -5,26 +5,6 @@ source "$DIR/config.sh"
 
 sudo BORG_PASSPHRASE="$BORG_PASSPHRASE" borg create \
   --one-file-system \
-  "$REPOSITORY_PATH"::$(date +"%Y-%m-%d-%H%M%S") \
-  /bin \
-  /boot \
-  /etc \
-  /home \
-  /lib \
-  /lib32 \
-  /lib64 \
-  /libx32 \
-  /opt \
-  /root \
-  /sbin \
-  /snap \
-  /srv \
-  /usr \
-  /var/backups \
-  /var/lib \
-  /var/local \
-  /var/log \
-  /var/mail \
-  /var/metrics \
-  /var/opt \
-  /var/snap
+  --exclude-from excluded_paths.txt \
+  "$REPOSITORY_PATH::$(date +'%Y-%m-%d-%H%M%S')" \
+  /mnt/c 
